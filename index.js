795b7cb17ca2e9fd97668c010199cd9f7462a6ce
Mitchelle <104675426+iamanduru@users.noreply.github.com>
2024-11-03 13:28:07 +0200

    Single Post is Complete. Remaining the backend bit

--- index.js
+++
@@ -1,5 +0,0 @@
-function toggleSidebar() {
-    const sidebarOverlay = document.getElementById("sidebar");
-    sidebarOverlay.classList.toggle('active');
-    console.log("Sidebar toggle function triggered"); // Debugging statement
-}
